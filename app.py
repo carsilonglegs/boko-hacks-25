69565ef3087366f94495d3c6d588bdf5dff19ed3
Raqeeb Afolabi <bsc91@txstate.edu>
2025-01-22 10:54:45 -0800

    switched order of login bp and hub bp

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from routes.home import home_bp
-from routes.login import login_bp
 from routes.hub import hub_bp
+from routes.login import login_bp
+
+
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
 # Register Blueprints
 app.register_blueprint(home_bp)
-app.register_blueprint(login_bp)
 app.register_blueprint(hub_bp)
+app.register_blueprint(login_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
