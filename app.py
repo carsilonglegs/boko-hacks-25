2ecb9b8abbef9f1898845eddc693d166fdf5a7e1
Raqeeb Afolabi <bsc91@txstate.edu>
2025-01-22 06:54:13 -0800

    added hub BP stuff to app.py changed hub redierection
    changed hub redirection back to hub.hub in login.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from routes.home import home_bp
 from routes.login import login_bp
+from routes.hub import hub_bp
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -8,6 +9,7 @@ app.secret_key = "supersecretkey"
 # Register Blueprints
 app.register_blueprint(home_bp)
 app.register_blueprint(login_bp)
+app.register_blueprint(hub_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
